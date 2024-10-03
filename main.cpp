fece6051746acb1bea2dbc6d0609d9e5770eb663
Bezuglyj Vitaly <bevidm@yandex.ru>
2024-10-03 21:24:05 +0300

    WIP(io): Add read lines code

--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
 #include "lib.h"
 
-#include <iostream>
+void greet() {
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "IP filter tool" << std::endl;
+    std::cout << "Version: " << version() << std::endl;
+}
 
-int main(int, char **) {
-	std::cout << "Version: " << version() << std::endl;
-	std::cout << "Hello, world!" << std::endl;
-	return 0;
+int main(int, char**) {
+    greet();
+
+    std::string buffer;
+    std::vector<std::string> lines_buff;
+
+    while (getline(std::cin, buffer)) {
+        if (buffer.empty()) {
+            break;
+        }
+        lines_buff.push_back(buffer);
+    }
+
+    std::cout << "You entered the following lines: " << std::endl;
+    for (const auto& it : lines_buff) {
+        std::cout << it << std::endl;
+    }
+    return 0;
 }
