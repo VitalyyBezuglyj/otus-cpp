093f1fc4db3997038fbc3a73cb5b764a174be75d
Bezuglyj Vitaly <bevidm@yandex.ru>
2024-10-19 21:43:13 +0300

    fix(tests): Fix incorrect file path for ipfilter test

--- tests/gtest_ipfilter.cpp
+++ tests/gtest_ipfilter.cpp
@@ -9,7 +9,7 @@
 
 #include "ipfilter/ipfilter.h"
 
-const std::string TEST_DATA_PATH = "data/ip_filter.tsv";
+const std::string TEST_DATA_PATH = "tests/data/ip_filter.tsv";
 const std::string SOLUTION_HASH_STR =
     "9c14006ec1c4b0210308e5da2a24a71ea0ff09c87f96dba122b16e47becccdcf";
 
@@ -31,7 +31,7 @@ std::string calculateSHA3_256(const std::stringstream& ss) {
 
     // Convert digest to a hex string
     std::stringstream sha3string;
-    sha3string << std::hex << std::uppercase;
+    sha3string << std::hex;
     for (int i = 0; i < SHA256_DIGEST_LENGTH; ++i) {
         sha3string << std::setw(2) << std::setfill('0')
                    << static_cast<int>(digest[i]);
@@ -40,44 +40,22 @@ std::string calculateSHA3_256(const std::stringstream& ss) {
     return sha3string.str();  // Return the SHA3-256 hex string
 }
 
-std::string toLowercase(const std::string& input) {
-    std::string result = input;
-    std::transform(result.begin(), result.end(), result.begin(), ::tolower);
-    return result;
-}
-
-TEST(SHA3Test, CompareSHA3Hash) {
-    // Setup: Create a stringstream with the contents to hash
-    std::stringstream ss;
-    ss << "Hello, world!";
-
-    // Calculate the SHA3-256 hash
-    std::string calculated_hash = toLowercase(calculateSHA3_256(ss));
-
-    // Compare the calculated hash with the expected (ground truth) hash
-    EXPECT_EQ(calculated_hash, expected_sha3_hash);
-}
-
 TEST(CheckFilter, FilterPassesGivenTestCase) {
-    std::ifstream file(TEST_DATA_PATH);
+    auto test_data_path = fs::current_path() / TEST_DATA_PATH;
+    std::ifstream file(test_data_path.string());
 
     if (!file) {
-        std::cerr << "Unable to open file!" << std::endl;
+        FAIL() << "Failed to open file: " << TEST_DATA_PATH
+               << "Current  path: " << fs::current_path();
     }
 
-    std::stringstream buffer_in;
     std::stringstream buffer_out;
 
-    buffer_in << file.rdbuf();
+    // Call the filter function
+    ipfilter(file, buffer_out);
     file.close();
 
-    // Call the filter function
-    ipfilter(buffer_in, buffer_out);
-
-    std::string buffer_out_str = buffer_out.str();
-    std::cout << buffer_out_str;
-
-    auto answer_hash_str = toLowercase(calculateSHA3_256(buffer_out));
+    auto answer_hash_str = calculateSHA3_256(buffer_out);
 
     EXPECT_EQ(SOLUTION_HASH_STR, answer_hash_str);
 }
